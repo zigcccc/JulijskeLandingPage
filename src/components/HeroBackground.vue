4cf6442334709639090d3e652a902585e7e40209
Žiga Krašovec <zkrasovec@gmail.com>
2018-07-03 09:18:23 +0200

    Added Math.floor() to all pixel related parallax values

--- src/components/HeroBackground.vue
+++ src/components/HeroBackground.vue
@@ -4,13 +4,19 @@
     <img :style="frontGroundStyle" id="front-ground" class="hero-mountain" src="@/assets/spredaj.png" alt="Julian Alps">
     <div id="clouds">
       <div class="hero-clouds" id="cloud1">
-        <img src="/images/Clouds_01.png" alt="Clouds in Julian Alps">
+        <img :style="cloudsStyle1" src="/images/Clouds_01.png" alt="Clouds in Julian Alps">
       </div>
       <div class="hero-clouds" id="cloud2">
-        <img src="/images/Clouds_02.png" alt="Clouds in Julian Alps">
+        <img :style="cloudsStyle2" src="/images/Clouds_02.png" alt="Clouds in Julian Alps">
       </div>
       <div class="hero-clouds" id="cloud3">
-        <img src="/images/Clouds_03.png" alt="Clouds in Julian Alps">
+        <img :style="cloudsStyle3" src="/images/Clouds_03.png" alt="Clouds in Julian Alps">
+      </div>
+      <div class="hero-clouds" id="cloud4">
+        <img :style="cloudsStyle4" src="/images/Clouds_03.png" alt="Clouds in Julian Alps">
+      </div>
+      <div class="hero-clouds" id="cloud5">
+        <img :style="cloudsStyle5" src="/images/Clouds_02.png" alt="Clouds in Julian Alps">
       </div>
     </div>
   </div>
@@ -35,6 +41,21 @@ export default {
       frontGroundStyle: {
         perspective: '500px',
         transform: 'translate3d(0,0,0)'
+      },
+      cloudsStyle1: {
+        transform: 'translate3d(0,0,0)'
+      },
+      cloudsStyle2: {
+        transform: 'translate3d(0,0,0)'
+      },
+      cloudsStyle3: {
+        transform: 'translate3d(0,0,0)'
+      },
+      cloudsStyle4: {
+        transform: 'translate3d(0,0,0)'
+      },
+      cloudsStyle5: {
+        transform: 'translate3d(0,0,0)'
       }
     }
   },
@@ -43,9 +64,14 @@ export default {
       if (scrollTop < this.heroHeight) {
         let factor1 = window.map(scrollTop, 0, document.querySelector('body').clientHeight, 0, 2)
         let factor2 = window.map(scrollTop, 0, document.querySelector('body').clientHeight, 0, this.heroHeight / 4)
-        this.middleGroundStyle.transform = `translate3d(0, -${factor2}%, ${factor1}px) scale(${(factor1 / 10) + 1})`
-        this.middleGroundStyle.filter = `blur(${factor2}px) contrast(120%)`
+        this.middleGroundStyle.transform = `translate3d(0, 0, ${Math.floor(factor1)}px) scale(${(factor1 / 2) + 1})`
+        this.middleGroundStyle.filter = `blur(${Math.floor(factor2)}px) contrast(120%)`
         this.frontGroundStyle.transform = `translate3d(0, ${factor2 * 2}%, 0) scale(${(factor1) + 1})`
+        this.cloudsStyle1.transform = `translate3d(0,${Math.floor(scrollTop / 3)}px, 0)`
+        this.cloudsStyle2.transform = `translate3d(0,${Math.floor(scrollTop / 5)}px, 0)`
+        this.cloudsStyle3.transform = `translate3d(0,${Math.floor(scrollTop / 4)}px, 0)`
+        this.cloudsStyle4.transform = `translate3d(0,${Math.floor(scrollTop / 10)}px, 0)`
+        this.cloudsStyle5.transform = `translate3d(0,${Math.floor(scrollTop / 2)}px, 0)`
       }
     }
   },
@@ -70,6 +96,7 @@ export default {
   //+simpleTranstion
   &#middle-ground
     z-index: 5
+    transform-origin: bottom center
   &#front-ground
     z-index: 20
     filter: contrast(130%)
@@ -77,14 +104,36 @@ export default {
 
 #clouds
   #cloud1
-    transform: translate3d(5%, 30%, 0)
-    //background: red
+    width: 250%
+    left: -5%
+    animation: clouds-anim-4 ease-in-out 20s infinite
+
   #cloud2
-    transform: translate3d(0, 0, 0)
-    //background: blue
+    width: 200%
+    left: -75%
+    bottom: 10%
+    animation: clouds-anim-3 ease-in-out 35s infinite
+
   #cloud3
-    transform: translate3d(5%, 30%, 0)
-    //background: green
+    width: 150%
+    left: -25%
+    bottom: 8%
+    animation: clouds-anim-2 ease-in-out 25s infinite
+    
+  #cloud4
+    z-index: 50
+    width: 100%
+    left: -20%
+    bottom: -7%
+    animation: clouds-anim-1 ease-in-out 20s infinite
+  #cloud5
+    z-index: 50
+    width: 50%
+    right: 5%
+    bottom: -15%
+    animation: clouds-anim-3 ease-in-out 30s infinite
+    animation-delay: 200ms
+
   .hero-clouds
     z-index: 10
     position: absolute
@@ -92,10 +141,43 @@ export default {
     display: flex
     justify-content: center
     width: 100%
+    height: auto
     & > img
       max-height: 100%
       width: calc($container-width + 25%)
       object-fit: contain
+
+@keyframes clouds-anim-1
+  0%
+    transform: translateX(0)
+  50%
+    transform: translateX(5%)
+  100%
+    transform: translateX(0)
+
+@keyframes clouds-anim-2
+  0%
+    transform: translateY(0)
+  50%
+    transform: translateY(-15%)
+  100%
+    transform: translateY(0)
+
+@keyframes clouds-anim-3
+  0%
+    transform: translateX(0)
+  50%
+    transform: translateX(10%)
+  100%
+    transform: translateX(0)
+
+@keyframes clouds-anim-4
+  0%
+    transform: translateY(0)
+  50%
+    transform: translateY(-10%)
+  100%
+    transform: translateY(0)
 </style>
 
 
