67b78265a0a18a96c0c3f8d3690e0b08ea3b9646
Žiga Krašovec <zkrasovec@gmail.com>
2018-07-06 09:48:32 +0200

    Added aditional photos for Jesenice and Kranjska Gora, added basic styling for TNP section

--- src/components/TriglavNationalPark.vue
+++ src/components/TriglavNationalPark.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="triglav-national-park">test</div>
+  <div id="triglav-national-park">
+    <h2>Triglav National Park</h2>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,14 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+#triglav-national-park
+  min-height: 100vh
+  position: relative
+  z-index: 10005
+  background: $white
 
+  h2
+    font-weight: 900
+    color: $black
+    font-size: 3.5em
 </style>
