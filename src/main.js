8d1bf1ef53f26614690ee3b9e7ce58a9252e8601
Žiga Krašovec <zkrasovec@gmail.com>
2018-07-31 11:31:38 +0200

    Initialized facebook pixel code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueAnalytics from 'vue-analytics';
+import VueFacebookPixel from 'vue-analytics-facebook-pixel';
 import VeeValidate from 'vee-validate';
 import store from './store';
 
@@ -11,6 +12,8 @@ Vue.config.productionTip = false;
 Vue.use(VueAnalytics, {
 	id: 'UA-122922765-1'
 });
+
+Vue.use(VueFacebookPixel);
 
 new Vue({
 	store,
